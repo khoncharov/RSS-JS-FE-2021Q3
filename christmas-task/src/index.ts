4c0f3136c34680739e5dffa9779fc1c87e530e43
Konstantin Honcharov <k.honcharov@gmail.com>
2021-12-13 14:43:10 +0300

    init: start christmas task, add initial env configuration

---
+++ christmas-task/src/index.ts
@@ -0,0 +1,9 @@
+import './style.css';
+import './style.scss';
+import rssLogo from './assets/svg/rs_school_js.svg';
+
+const appContainer = document.querySelector('.app') as HTMLElement;
+appContainer.textContent = 'Template';
+const img = document.createElement('img');
+img.src = rssLogo;
+appContainer.appendChild(img);
---
+++ christmas-task/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[hash][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      favicon: path.resolve(__dirname, './src/favicon.ico'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode
+    ? require('./webpack.prod.config')
+    : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
